c2bf6ceed92070e50699e9c0c2639f0c2c4055de
alex-reiff <alex-reiff@users.noreply.github.com>
2025-07-03 16:06:50 +0000

    deploy: 784cdea5c08b1f52f14ae2dda144f61453500026

--- client/src/components/forms/instance-forms/access-groups/index.js
+++
@@ -1,5 +0,0 @@
-import AccessGroupDynamicPolicyForm from "./AccessGroupDynamicPolicyForm.js";
-import AccessGroupForm from "./AccessGroupForm.js";
-import AccessGroupPolicyForm from "./AccessGroupPolicyForm.js";
-
-export { AccessGroupDynamicPolicyForm, AccessGroupForm, AccessGroupPolicyForm };
